34c0e02231dec44583f0aac0d2c8cbb04ec59656
Hashimoto2 <arsnyui2@gmail.com>
2020-12-19 05:21:39 +0000

    修正

---
+++ app/controllers/admins/end_user_controller.rb
@@ -0,0 +1,18 @@
+class Admins::EndUserController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
   namespace :admins do
     resources :articles
     resources :categories, only: [:index, :create, :edit, :update]
+    resources :end_user, only: [:index, :show, :edit, :update]
   end
 
   root 'customers/articles#top'

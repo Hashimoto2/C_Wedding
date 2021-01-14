bea67bc200c51008e7e20363d142e1a72beefdbe
Hashimoto2 <arsnyui2@gmail.com>
2021-01-14 13:20:59 +0000

    修正

--- app/controllers/customers/articles_controller.rb
+++ app/controllers/customers/articles_controller.rb
@@ -49,8 +49,8 @@ class Customers::ArticlesController < ApplicationController
     @comment = Comment.new
     @comments = @article.comments
     @categories = Category.where(is_active: true)
-     # 参照先のS3オブジェクトURLを作成
-    @article_url = "https://c-wedding-s3.s3-us-east-1.amazonaws.com/store/" + @article.image_id + "-thumbnail."
+    # 参照先のS3オブジェクトURLを作成
+    # @article_url = "https://c-wedding-s3.s3-us-east-1.amazonaws.com/store/" + @article.image_id + "-thumbnail."
   end
 
   def edit

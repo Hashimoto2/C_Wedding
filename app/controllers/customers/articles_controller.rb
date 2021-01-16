94b47d49efd7bcc3609b75119f251e4ea7cce6d1
Hashimoto2 <arsnyui2@gmail.com>
2021-01-16 07:15:46 +0000

    修正

--- app/controllers/customers/articles_controller.rb
+++ app/controllers/customers/articles_controller.rb
@@ -50,9 +50,7 @@ class Customers::ArticlesController < ApplicationController
     @comments = @article.comments
     @categories = Category.where(is_active: true)
     #参照先のS3オブジェクトURLを作成
-    pp @article.image_id
-    @article_url = "https://c-wedding-s3-resize.s3-us-east-1.amazonaws.com/store/" + @article.image_id.to_s + "-thumbnail."
-    pp @article_url
+    @article_url = "https://#{ENV['AWS_S3_BUCKET_NAME']}-resize.s3.amazonaws.com/store/#{@article.image_id.to_s}-thumbnail."
   end
 
   def edit
--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,7 +4,7 @@ aws = {
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   region: "us-east-1",
-  bucket: "c-wedding-s3",
+  bucket: ENV["AWS_S3_BUCKET_NAME"]
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
